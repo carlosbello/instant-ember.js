0b5eeec7806ebf74dea84cb0e313b66b260c7036
bellopauste <bellopauste@yahoo.com>
2014-10-27 19:59:07 +0100

    Creating an Ember controller done

--- js/controllers/application.js
+++ js/controllers/application.js
@@ -1,1 +1,26 @@
 MovieTracker.ApplicationController = Ember.Controller.extend();
+
+MovieTracker.moviesController = Ember.ArrayController.create({
+    content: [],
+    init: function () {
+        // HACK! Change for Ember 1.7: Removed the this _super() call because a deprecation error message
+        // this dog _super();
+        
+        var list = [
+            MovieTracker.Movie.create({title: 'Movie 1', rating: 4}),
+            MovieTracker.Movie.create({title: 'Movie 2', rating: 5})
+        ];
+        this.set('content', list);
+    }
+});
+
+MovieTracker.selectedMovieController = Ember.ObjectController.create({
+    selectedMovie: [],
+    
+    select: function (item) {
+        this.set('selectedMovie', item);
+    },
+    toogleWatched: function () {
+        this.selectedMovie.toogleProperty('watched');
+    }
+});
