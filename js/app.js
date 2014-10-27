7fd7b896b5342d7f2db651e9b1e4ad79cb6309f1
bellopauste <bellopauste@yahoo.com>
2014-10-27 19:26:50 +0100

    Enhancing an Ember object done

--- js/app.js
+++ js/app.js
@@ -1,14 +1,1 @@
 var MovieTracker = Ember.Application.create();
-/*
-App = Ember.Application.create();
-
-App.Router.map(function() {
-  // put your routes here
-});
-
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
-*/
--- js/models/movie.js
+++ js/models/movie.js
@@ -1,10 +1,25 @@
-MovieTracker.Movie = Ember.Object.extend({
+MovieTracker.WatchedMixin = Ember.Mixin.create({
+    isWatched: function () {
+        var title = this.get('title'),
+            watched = this.get('watched');
+        return ('Has ' + title + ' been watched? ' + watched);
+    }
+});
+
+MovieTracker.Movie = Ember.Object.extend(MovieTracker.WatchedMixin, {
     id: null,
     tittle: null,
     watched: false,
-    rating: 0
+    rating: 0,
+    titleAndRating: function () {
+        return this.get('title') + ' has a rating of ' + this.get('rating');
+    }.property('title', 'rating'),
+    titleChanged: function () {
+        console.log('Title changed!');
+    }.observes('title')
 });
 
 MovieTracker.ActionMovie = MovieTracker.Movie.extend({
     genere: "action"
 });
+
